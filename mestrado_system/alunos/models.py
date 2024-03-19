59b67fc7d5553d1e4a4c07dfa33bad682567da2d
anajulia <anajcaetanom@gmail.com>
2024-03-19 14:00:16 -0300

    new file:   mestrado_system/alunos/migrations/0020_alter_aluno_defesa_alter_aluno_obs.py 	modified:   mestrado_system/alunos/models.py 	modified:   mestrado_system/db.sqlite3

--- mestrado_system/alunos/models.py
+++ mestrado_system/alunos/models.py
@@ -6,11 +6,11 @@ class Aluno(models.Model):
     sobrenome = models.CharField(max_length=255)
     curso = models.ForeignKey(Turma, on_delete=models.SET_NULL, null=True)
     orientadores = models.ManyToManyField('docentes.Docente', related_name='alunos_orientados')
-    defesa = models.BooleanField(default=False)
+    defesa = models.BooleanField(default=False, blank=True)
     data_defesa = models.DateField(null=True)
     artigo = models.URLField(max_length=400, null=True, blank=True)
     bolsista = models.CharField(max_length=255)
-    obs = models.TextField(max_length=255)
+    obs = models.TextField(max_length=255, blank=True)
     email = models.EmailField(max_length=255, null=True)
     matricula = models.CharField(max_length=20, null=True)
 
